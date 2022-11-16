59adf725bf9cb5f9dd7380538eb7b71cc4160a83
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2022-11-16 09:40:13 -0600

    Adding logic to read index.html and style.css templates and create the new files with the proper data

--- index.js
+++ index.js
@@ -86,12 +86,76 @@ const internquestions = [
 
 ];
 
+
+function appendInitialData(){
+    const content = fs.readFileSync("./src/srcindex.html", "utf8");
+    fs.appendFileSync("./dist/index.html", content);
+}
+
+function addCard(object){
+    const Name = object.getName();
+    const Id = object.getId();
+    const Email = object.getEmail();
+    const Role = object.getRole();
+    let icon = "";
+    let thirdlistitem = "";
+
+    if(Role == "Manager"){
+        const Office = object.OfficeNumber;
+        icon = "<i class=\"bi bi-cup-hot\"></i>";
+        thirdlistitem = "Office number: " + Office;
+    } else if (Role == "Engineer"){
+        const GitHub = object.getGithub();
+        icon = "<i class=\"bi bi-eyeglasses\"></i>";
+        thirdlistitem = `GitHub: <a href="https://github.com/${GitHub}">${GitHub}</a>`;
+    } else if (Role == "Intern"){
+        const School = object.getSchool();
+        icon = "<i class=\"bi bi-mortarboard\"></i>";
+        thirdlistitem = "School: " + School;
+    }
+
+    let textcard = 
+    `\n<div class="card" style="width: 320px;">
+        <div class="card-body bg-primary text-white">
+            <h5 class="card-title">${Name}</h5>
+            <p class="card-text">${icon} ${Role}</p>
+        </div>
+        <div class="List">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${Id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${Email}">${Email}</a></li>
+                <li class="list-group-item">${thirdlistitem}</li>
+            </ul>
+        </div>
+    </div>`;
+
+    fs.appendFileSync("./dist/index.html", textcard);
+
+}
+
+function appendFinalData(){
+    const finaltext = 
+    ` \n</main>
+    </body>
+  </html>`;
+    fs.appendFileSync("./dist/index.html", finaltext);
+}
+
+function createCSSFile(){
+    const styletext = fs.readFileSync("./src/srcstyle.css", "utf8");
+    fs.writeFileSync("./dist/style.css", styletext);
+}
+
+
+
 function init(){
     inq.prompt(managerquestions)
     .then((answers) => {
         const manager = new Manager(answers.Name, answers.Id, answers.Email, answers.Office);
         console.log(manager.OfficeNumber);
         console.log(manager.getRole());
+        appendInitialData();
+        addCard(manager);
         displayChoices();
     });
 };
@@ -108,7 +172,7 @@ function displayChoices(){
                 const engineer = new Engineer(answers.Name, answers.Id, answers.Email, answers.GitHub);
                 console.log(engineer.getGithub());
                 console.log(engineer.getRole());
-                //renderhtml(engineer);
+                addCard(engineer);
                 displayChoices();
             });
 
@@ -120,16 +184,20 @@ function displayChoices(){
                 const intern = new Intern(answers.Name, answers.Id, answers.Email, answers.School);
                 console.log(intern.getSchool());
                 console.log(intern.getRole());
-                //renderhtml(intern);
+                addCard(intern);
                 displayChoices();
             });
             
         }else if(answers.Option == "Finish team building process"){
 
             console.log("Finish process");
-            //createhtmlfile();
+            appendFinalData();
+            createCSSFile();
         }
     });
 }
+
+
+
 
 init();
